f34a886a812cba456d1888474b007ce45f53a2b2
satvikDesktop <satviksingh2304@gmail.com>
2022-06-03 10:21:59 +0530

    added location details

--- map.js
+++ map.js
@@ -6,6 +6,7 @@
     )
     .then ((data) => {
       coordinates(data)
+      display(data)
     })
     .catch (err=>console.log(err)) 
 
@@ -28,7 +29,33 @@ function coordinates (data) {
           center: ol.proj.fromLonLat([data.location.longitude,data.location.latitude]),
           zoom: 12
         })
-      });
-    }
+    });
+}
+function display(data) {
+  var ip= document.createElement("h2");
+  ip.textContent=`Your I.P. address is : ${data.ip}`
+
+  var country= document.createElement("h2");
+  country.textContent=`Country : ${data.country.name} , ${data.country.name_native}`
+
+  var city= document.createElement("h2");
+  city.textContent=`City :${ data.city.name}`
+
+  var currency= document.createElement("h2");
+  currency.textContent=`Currency : ${data.country.currency}`
+
+  var post= document.createElement("h2");
+  post.textContent=`Postal Code : ${data.postcode}`
+
+  var phn= document.createElement("h2");
+  phn.textContent=`Phone Code : ${data.country.phone_code}`
+
+  document.getElementById('details').appendChild(ip);
+  document.getElementById('details').appendChild(country);
+  document.getElementById('details').appendChild(city);
+  document.getElementById('details').appendChild(post);
+  document.getElementById('details').appendChild(currency);
+  document.getElementById('details').appendChild(phn);
+}
 
       
